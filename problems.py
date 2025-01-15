235444654ef831aa89d4a5c0ab153280c07d4125
kattenlaf <ruelgordon123@gmail.com>
2025-01-14 20:26:16 -0800

    few new problems

--- problems.py
+++ problems.py
@@ -838,6 +838,48 @@ class Solution:
                         maximums[2] = nums[i]
         return maximums[2] if maximums[2] != "" else maximums[0]
 
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
+
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s_list = s.split(" ")
+        hashmap = {}
+        hashset = set()
+        if len(s) != len(s_list):
+            return False
+        for i in range(len(pattern)):
+            if pattern[i] not in hashmap:
+                # If the current letter has not been seen but the word has return false, that means multiple mappings
+                if s_list[i] in hashset:
+                    return False
+                hashmap[pattern[i]] = s_list[i]
+                hashset.add(s_list[i])
+            else:
+                if hashmap[pattern[i]] != s_list[i]:
+                    return False
+
+        return True
+
+    def findPeakElement(self, nums: List[int]) -> int:
+        # can solve using binary search
+        l, r = 0, len(nums) - 1
+        while l < r:
+            # if mid is > index to the right, we know peak will be over there otherwise peak will be to the left
+            #O(logn)
+            mid = (r + l) // 2
+            if nums[mid] > nums[mid + 1]:
+                r = mid
+            else:
+                l = mid + 1
+
+        return l
+
 def buildList(nums):
     dummy = ListNode()
     current = dummy
@@ -859,7 +901,7 @@ test = defaultdict(list)
 # print(solutions.canConstruct("aa", "aab"))
 # print(solutions.findMaxAverage([1,12,-5,-6,50,3], 4))
 # print(solutions.findKthLargest([3,2,1,5,6,4], 2))
-print(solutions.thirdMax([5,2,2]))
+print(solutions.findPeakElement([1,2,1,3,5,6,4]))
 
 """
 list1 = buildList([1,4,5])
